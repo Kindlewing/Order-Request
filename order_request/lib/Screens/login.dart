d9516e91ec0aca61821021640a639e26eb3ded70
Hudson Finn <Hudson.Finn@gordon.edu>
2021-12-21 16:45:06 -0500

    Working on authentication and forms. Also added some color themes at the root

--- order_request/lib/Screens/login.dart
+++ order_request/lib/Screens/login.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:order_request/Shared/constants.dart';
-
 
 class Login extends StatefulWidget {
   Login({Key? key}) : super(key: key);
@@ -25,38 +23,30 @@ class _LoginState extends State<Login> {
             padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
             child: Form(
                 child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 20.0),
-                    TextFormField(
-                        decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                        onChanged: (value) {
-                          print(value);
-                        }
-                    ),
-                    SizedBox(height: 20.0),
-                    TextFormField(
-                        obscureText: true,
-                        decoration: textInputDecoration.copyWith(hintText: 'Password'),
-                        onChanged: (value) {
-                          print(value);
-                        }
-                    ),
-                    SizedBox(height: 20.0),
-                    ElevatedButton(
-                      onPressed: () {
-
-                      },
-                      child: Text(
-                        'Login',
-                        style: Theme.of(context).textTheme.button,
-                      ),
-                    ),
-                  ],
-                )
-            )
-        )
-    );
+              children: <Widget>[
+                SizedBox(height: 20.0),
+                TextFormField(
+                    decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                    onChanged: (value) {
+                      print(value);
+                    }),
+                SizedBox(height: 20.0),
+                TextFormField(
+                    obscureText: true,
+                    decoration:
+                        textInputDecoration.copyWith(hintText: 'Password'),
+                    onChanged: (value) {
+                      print(value);
+                    }),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Login',
+                    style: Theme.of(context).textTheme.button,
+                  ),
+                ),
+              ],
+            ))));
   }
-
 }
-

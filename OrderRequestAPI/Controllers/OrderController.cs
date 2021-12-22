5317b4c8492eb539b7367dea6c7eb304985883b2
Hudson Finn <Hudson.Finn@gordon.edu>
2021-12-22 17:33:59 -0500

    added the API

---
+++ OrderRequestAPI/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using OrderRequestAPI.Models;
+
+namespace OrderRequestAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase {
+        [HttpGet]
+        public List<Order> GetOrders() {
+            List<Order> list = new List<Order>();
+            list.Add(new Order("2 PM", "Walmart"));
+            list.Add(new Order("1 PM", "Target"));
+            list.Add(new Order("10 AM", "Starbucks"));
+            return list;
+        }
+    }
+}
---
+++ OrderRequestAPI/Models/Order.cs
@@ -0,0 +1,13 @@
+
+
+namespace OrderRequestAPI.Models {
+    public class Order {
+        public string TimeLeaving { get; set; }
+        public string Destination { get; set; }
+
+        public Order(string timeLeaving, string destination) {
+            TimeLeaving = timeLeaving;
+            Destination = destination;
+        }
+    }
+}
